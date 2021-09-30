547c0eee4e5f168ecd1dff371feb43a028eea3d7
greatmartial <fananbang94@gmail.com>
2021-09-30 18:59:29 +0800

    [Feature] traverse the target event in the block and save it.

--- index.ts
+++ index.ts
@@ -1,80 +1,100 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { SignedBlock } from '@polkadot/types/interfaces/runtime';
+import * as fs from 'fs';
 
-import { eventsCount } from './utils';
+import eventsCount from './utils';
 
-async function main () {
-    // Kusama network
-    //const url: string = 'wss://kusama-rpc.polkadot.io';
-    // Polkadot network
-    const url: string = 'wss://rpc.polkadot.io';
-    const provider = new WsProvider(url);
+// eventsMap用于存储最后的event-number
+const eventsCountMap = new Map<string, number>();
+// eventsAddrMap用于存储event-address的tag
+const eventsAddrMap = new Map<string, boolean>();
 
-    // Create the API and wait until ready
-    const api = await ApiPromise.create({ provider });
+async function main() {
+  // Kusama network
+  const url = 'wss://kusama-rpc.polkadot.io';
+  // Polkadot network
+  // const url = 'wss://rpc.polkadot.io';
+  const provider = new WsProvider(url);
 
-    // Retrieve the chain & node information information via rpc calls
-    const [chain, nodeName, nodeVersion] = await Promise.all([
-        api.rpc.system.chain(),
-        api.rpc.system.name(),
-        api.rpc.system.version()
-    ]);
+  // Create the API and wait until ready
+  const api = await ApiPromise.create({ provider });
 
-    console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+  // Retrieve the chain & node information information via rpc calls
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version(),
+  ]);
 
-//   const blockHash = '0x37ee25393672ca9f91b700ac06996b5c165ece53a690bfa45e6dfbd92c6dd43e';
-//   const signedBlock = await api.rpc.chain.getBlock(blockHash);
-    const unsub = await api.query.timestamp.now((moment) => {
-        console.log(`The last block has a timestamp of ${moment}`);
-    });
+  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
 
-    const signedBlock = await api.rpc.chain.getBlock();
-    const blockNumberLatest = signedBlock.block.header.number.toNumber();
+  //   const blockHash = '0x37ee25393672ca9f91b700ac06996b5c165ece53a690bfa45e6dfbd92c6dd43e';
+  //   const signedBlock = await api.rpc.chain.getBlock(blockHash);
+  const unsub = await api.query.timestamp.now((moment) => {
+    console.log(`The last block has a timestamp of ${moment}`);
+  });
 
+  let signedBlock = await api.rpc.chain.getBlock();
+  const blockNumberLatest = signedBlock.block.header.number.toNumber();
 
-    let eventsMap = new Map<string, number>()
-    for (let i=7038600; i < blockNumberLatest; i++) {
-        const targetBlockHash = await api.rpc.chain.getBlockHash(i);
-        const signedBlock = await api.rpc.chain.getBlock(targetBlockHash);
-        await executeCount(eventsMap, api, signedBlock);
-        
-        console.log(`the #${i} block of event count: `, eventsMap)
-    }
+  for (let i = 0; i < blockNumberLatest; i++) {
+    const targetBlockHash = await api.rpc.chain.getBlockHash(i);
+    signedBlock = await api.rpc.chain.getBlock(targetBlockHash);
+    await executeCount(eventsCountMap, eventsAddrMap, api, signedBlock);
 
+    // console.log(`the #${i} block of event count: `, eventsCountMap, eventsAddrMap);
+    console.log(`the #${i} block of event count: `, eventsCountMap);
+  }
+  // save to files
+  const eventsCountJson = Object.fromEntries(eventsCountMap);
+  const eventsAddrJson = Object.fromEntries(eventsAddrMap);
 
+  const networkNameAsCount = './kusame-address-count.json';
+  const networkNameAsAddress = './kusama-address.json';
+
+  fs.writeFileSync(networkNameAsCount, JSON.stringify(eventsCountJson));
+  fs.writeFileSync(networkNameAsAddress, JSON.stringify(eventsAddrJson));
 }
 
 main().catch(console.error).finally(() => process.exit());
 
+async function executeCount(
+  eventsCountMap: Map<string, number>,
+  eventsAddrMap: Map<string, boolean>,
+  api: ApiPromise,
+  signedBlock: SignedBlock,
+): Promise<Map<string, number>> {
+  let promise: Promise<Map<string, number>>;
 
-async function executeCount(eventsMap: Map<string, number>, api: ApiPromise, signedBlock: SignedBlock): Promise<Map<string, number>> {
-    let promise: Promise<Map<string, number>>
+  console.log(`the block number of latest: #${signedBlock.block.header.number.toNumber()}`);
+  console.log(signedBlock.block.header.hash.toHex());
 
-    console.log(`the block number of latest: #${signedBlock.block.header.number.toNumber()}`);
-    console.log(signedBlock.block.header.hash.toHex());
-    
-    // the hash for each extrinsic in the block
-    signedBlock.block.extrinsics.forEach((ex, index) => {
-        console.log(index, ex.hash.toHex());
-    });
+  // the hash for each extrinsic in the block
+  signedBlock.block.extrinsics.forEach((ex, index) => {
+    console.log(index, ex.hash.toHex());
+  });
 
-    const allRecords = await api.query.system.events.at(signedBlock.block.header.hash);
+  const allRecords = await api.query.system.events.at(signedBlock.block.header.hash);
 
   // map between the extrinsics and events
-    signedBlock.block.extrinsics.forEach(({ method: { method, section } }, index) => {
-        const events = allRecords
-            .filter(({ phase }) =>
-                phase.isApplyExtrinsic &&
-                phase.asApplyExtrinsic.eq(index)
-            )
-            .map(({ event }) => `${event.section}.${event.method}`);
+  signedBlock.block.extrinsics.forEach((ex, index) => {
+    const { isSigned, meta, method: { args, method, section } } = ex;
 
-        console.log(`${section}.${method}:: ${events.join(', ') || 'no events'}`);
-        eventsCount(eventsMap, events);
-    });
+    const events = allRecords
+      .filter(({ phase }) => phase.isApplyExtrinsic
+                && phase.asApplyExtrinsic.eq(index))
+      .map(({ event }) => `${event.section}.${event.method}`);
 
-    promise = new Promise<Map<string, number>>(resolve => {
-        resolve(eventsMap)
-    })
-    return promise;
+    console.log(`${section}.${method}:: ${events.join(', ') || 'no events'}`);
+
+    if (isSigned) {
+      console.log(`signer=${ex.signer.toString()}, nonce=${ex.nonce.toString()}`);
+      eventsCount(eventsCountMap, eventsAddrMap, ex.signer.toString(), events);
+    }
+  });
+
+  promise = new Promise<Map<string, number>>((resolve) => {
+    resolve(eventsCountMap);
+  });
+  return promise;
 }
--- utils.ts
+++ utils.ts
@@ -1,25 +1,39 @@
+import * as fs from 'fs';
 
-export function eventsCount(eventMap: Map<string, number>, events: string[], filter?: string): Map<string, number> {
-    events.forEach((event, index) => {
-        // 对目标event进行筛选
-        const namelist = event.split('.');
-        console.log('Func eventsCount: namelist:: ', namelist[0]);
-        // TODO: 对event进行
-        switch (namelist[0]) {
-            case ('treasury' || 'democracy' || 'society' || 'council'):
-                console.log('target sccuess!!!')
-                const count = eventMap.get(event);
-                if (count !== undefined) {
-                    eventMap.set(event, count + 1);
-                } else {
-                    eventMap.set(event, 1)
-                }
-                break;
-            default:
-                break;
-            };               
+export default function eventsCount(
+  eventMap: Map<string, number>,
+  addrMap: Map<string, boolean>,
+  addr: string,
+  events: string[],
+): Map<string, number> {
+  events.forEach((event) => {
+    // 对目标event进行筛选
+    const namelist = event.split('.');
+    console.log('Func eventsCount: namelist:: ', namelist[0]);
+    // TODO: 对event进行
+    switch (namelist[0]) {
+      case ('treasury' || 'democracy' || 'society' || 'council' || 'society' || 'collective'):
+        console.log('target sccuess!!!');
+
+        // 判断target address是否已经存在
+        if (addrMap.get(`${event}=${addr}`) === undefined) {
+          // 判断target event是否已经存在
+          const count = eventMap.get(event);
+          if (count !== undefined) {
+            eventMap.set(event, count + 1);
+          } else {
+            eventMap.set(event, 1);
+          }
+          // 为该address相应的事件，打tag
+          addrMap.set((`${event}=${addr}`), true);
+          // 地址tag追加进文件，用于验证
+          fs.appendFileSync('kusama-eventAddr-count.json', `{"name": "${event}=${addr}"},\n`);
         }
-    );
-    
-    return eventMap;
+        break;
+      default:
+        break;
+    }
+  });
+
+  return eventMap;
 }
